04da6440528e52b09fd6959b53a8d058888ac6e6
Brandon Makin <BrandonMakin@users.noreply.github.com>
2023-10-27 21:37:59 -0700

    something

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Automatic Slideshow - change image every 4 seconds
-var slideIndex = 0;
 carousel();
 
-function carousel() {
+function carousel(slideIndex) {
+  slideIndex = slideIndex || 0; // set slideIndex to 0 if it was null
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
@@ -11,7 +11,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 4000);
+  setTimeout(() => carousel(slideIndex), 4000);
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
@@ -31,3 +31,128 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// populate #projects section with cards using the template
+let cardTemplate = document.querySelector("#project-card-template");
+let cardGrid = document.querySelector("#photo-grid");
+let cards = []
+let a = cardTemplate.content.cloneNode(true);
+
+async function initializeGrid() {
+  for (i = 0; i < 6; i++)
+  {
+    let card = cardTemplate.content.cloneNode(true);
+    cards[i] = card.children[0];
+    // cards[i].id = `project-card-${i}`; // add unique id (e.g. "project-card-1") to each card
+    cardGrid.appendChild(card) // add card to body
+  }
+  await fetchProjectData();
+  updateProjectGrid();
+}
+
+let projectData;
+let projectTags = new Set();
+
+async function fetchProjectData()
+{
+  let response = await fetch("projects.json");
+  projectData = await response.json();
+}
+
+function queryProjects(tags) // return a list of projects that include any element of the array `tags`
+{
+  if (tags.size == 0)
+    return projectData;
+  return projectData.filter(project => project.tags.some(projectTag => tags.has(projectTag)))
+}
+
+function updateProjectGridDisplay(projects) {
+  projectCount = Math.min(cards.length, projects.length); // show only as many projects as there are html cards
+  for (i = 0; i < cards.length; i++) { // for each card:
+    let card = cards[i];
+    if (i >= projectCount) { // extra card elements should be invisible
+      card.classList.add("w3-hide");
+
+    } else { // for the visible card elements
+      let project = projects[i]
+
+      // make card visible
+      card.classList.remove("w3-hide");
+
+      // display image
+      card.querySelector("img").src = "img/projects/" + project.thumbnail;
+
+      // display project name
+      card.querySelector(".card-name").textContent = project.name
+
+      // display description
+      card.querySelector(".card-desc").textContent = project.desc
+
+      // display tags
+      tagContainerElement = card.querySelector(".project-tags");
+      tagContainerElement.innerHTML = "";
+      // hard-code data each tag because that's easy and there are only 4 tags.
+      if (project.tags.includes("games"))
+        tagContainerElement.innerHTML += `<i class="project-icon fa fa-gamepad"></i>`;
+      if (project.tags.includes("code"))
+        tagContainerElement.innerHTML += `<i class="project-icon fa fa-code"></i>`;
+      if (project.tags.includes("art"))
+        tagContainerElement.innerHTML += `<i class="project-icon fa fa-palette"></i>`;
+      if (project.tags.includes("other"))
+        tagContainerElement.innerHTML += `<i class="project-icon fa fa-frog"></i>`;
+
+    }
+  }
+}
+
+function updatePaginationDisplay()
+{
+
+}
+
+function updateProjectGrid() {
+    if (projectData) {
+      let projects = queryProjects(projectTags);
+      updateProjectGridDisplay(projects);
+      updatePaginationDisplay(projects.length);
+    }
+}
+
+function toggleTag(event, tag) {
+  if (projectTags.has(tag)) {
+    projectTags.delete(tag);
+    event.target.classList.add("w3-light-grey");
+    event.target.classList.remove("w3-black");
+  }
+  else {
+    projectTags.add(tag);
+    event.target.classList.remove("w3-light-grey");
+    event.target.classList.add("w3-black");
+  }
+  let buttonAll = document.querySelector("#filter-all");
+  if (projectTags.size == 0)
+  {
+    buttonAll.classList.add("w3-black");
+    buttonAll.classList.remove("w3-light-grey");
+  } else
+  {
+    buttonAll.classList.remove("w3-black");
+    buttonAll.classList.add("w3-light-grey");
+  }
+  updateProjectGrid();
+}
+
+function enableAllTags()
+{
+  projectTags.clear()
+  let buttonAll = document.querySelector("#filter-all")
+  document.querySelectorAll(".filters > button").forEach(button => {
+    button.classList.remove("w3-black");
+    button.classList.add("w3-light-grey");
+  })
+  buttonAll.classList.add("w3-black");
+  buttonAll.classList.remove("w3-light-grey");
+  updateProjectGrid();
+}
+
+initializeGrid();
